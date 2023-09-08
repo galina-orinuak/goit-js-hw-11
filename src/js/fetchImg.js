23e86d34144951edf36a8df0d622052f6a0656ce
kruhitka1706 <kruhitka1706@gmail.com>
2023-09-08 14:08:46 +0300

    Update fetchImg.js

--- src/js/fetchImg.js
+++ src/js/fetchImg.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
+const pageLimit = 40;
+
 const fetchImages = async (queryToFetch, pageToFetch) => {
 
     const BASE_URL = 'https://pixabay.com/api/';
     const API_KEY = '39241175-920eb89e79fcd4cc7c8ced0a6';
-    const pageLimit = 40;
+    
 
      const params = new URLSearchParams({
         key: API_KEY,
